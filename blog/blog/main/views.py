d04134f03ddefb22784d69dbf75af386047379f4
OffImagination <66019113+ZhukovAlexander191@users.noreply.github.com>
2021-07-22 18:37:49 +0500

    Auth

--- blog/blog/main/views.py
+++ blog/blog/main/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
+from .forms import RegUserForm
 
 def login(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+# Добавить страницу аккаунта
+        if user is not None:
+            login(request, username)
+            redirect('')
+
+    context = {}
     return render(request, 'main/log.html')
+
+
+
+def register(request):
+    form = RegUserForm()
+
+    if request.method == 'POST':
+        form = RegUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login', )
+
+    context = {'form': form}
+    return render(request, 'main/reg.html', context)
+
+
+def blog_main(request):
+    pass
