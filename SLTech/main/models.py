be692428fb17cba12549aa6ac943cc7388d9e98e
OffImagination <66019113+ZhukovAlexander191@users.noreply.github.com>
2021-08-01 18:52:05 +0500

    2.2

--- SLTech/main/models.py
+++ SLTech/main/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 class Article(models.Model):
-    pass
+    art_title = models.CharField('Название статьи', max_length=125)
+    art_text = models.TextField('Содержание статьи')
+    pub_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.art_title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
 
 
--- SLTech/main/views.py
+++ SLTech/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def index(request):
     return render(request, 'main/index.html')
 
 
+@login_required(login_url='login')
 def about(request):
     return render(request, 'main/about.html')
